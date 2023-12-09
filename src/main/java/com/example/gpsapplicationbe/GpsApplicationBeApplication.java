efe4d2a912902cfedb984e5a01278a5208f5df93
Faruk <fceric12@gmail.com>
2023-12-09 17:43:54 +0100

    first commit

---
+++ src/main/java/com/example/gpsapplicationbe/GpsApplicationBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.gpsapplicationbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpsApplicationBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpsApplicationBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gpsapplicationbe/GpsApplicationBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gpsapplicationbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpsApplicationBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
